80858784e9db784cd9b35d76cf3ebb8ecaec9d29
alexcam96 <s1071182@studenti.univpm.it>
2017-05-13 12:36:46 +0200

    LoadModificheProj

---
+++ prog_tw/application/controllers/FaqController.php
@@ -0,0 +1,18 @@
+<?php
+
+class FaqController extends Zend_Controller_Action {
+
+
+
+    public function init() {
+        $this->_helper->layout->setLayout('main');
+
+    }
+    
+    public function visualizzaAction() {
+    
+          $this->view->assign(array('text' => "LOREM IPSUM"));
+    }
+    
+    
+}
--- prog_tw/application/controllers/PublicController.php
+++ prog_tw/application/controllers/PublicController.php
@@ -2,20 +2,20 @@
 
 class PublicController extends Zend_Controller_Action {
 
-    protected $_descprod = '<p>Sed lacus. Donec lectus. Nullam pretium nibh ut turpis. Nam bibendum. In nulla tortor, elementum vel, tempor at, varius non, purus. Mauris vitae nisl nec metus placerat consectetuer. Donec ipsum. Proin imperdiet est. Phasellus dapibus semper urna. Pellentesque ornare, orci in consectetuer hendrerit, urna elit eleifend nunc, ut consectetuer nisl felis ac diam. Etiam non felis. Donec ut ante. In id eros. Suspendisse lacus turpis, cursus egestas at sem. Phasellus pellentesque. Mauris quam enim, molestie in, rhoncus ut, lobortis a, est. </p><p>Sed lacus. Donec lectus. Nullam pretium nibh ut turpis. Nam bibendum. In nulla tortor, elementum vel, tempor at, varius non, purus. Mauris vitae nisl nec metus placerat consectetuer. Donec ipsum. Proin imperdiet est. Phasellus dapibus semper urna. Pellentesque ornare, orci in consectetuer hendrerit, urna elit eleifend nunc, ut consectetuer nisl felis ac diam. Etiam non felis. Donec ut ante. In id eros. Suspendisse lacus turpis, cursus egestas at sem. Phasellus pellentesque. Mauris quam enim, molestie in, rhoncus ut, lobortis a, est.</p>';
+    protected $_testo = '<p>Sed lacus. Donec lectus. Nullam pretium nibh ut turpis. Nam bibendum. In nulla tortor, elementum vel, tempor at, varius non, purus. Mauris vitae nisl nec metus placerat consectetuer. Donec ipsum. Proin imperdiet est. Phasellus dapibus semper urna. Pellentesque ornare, orci in consectetuer hendrerit, urna elit eleifend nunc, ut consectetuer nisl felis ac diam. Etiam non felis. Donec ut ante. In id eros. Suspendisse lacus turpis, cursus egestas at sem. Phasellus pellentesque. Mauris quam enim, molestie in, rhoncus ut, lobortis a, est. </p><p>Sed lacus. Donec lectus. Nullam pretium nibh ut turpis. Nam bibendum. In nulla tortor, elementum vel, tempor at, varius non, purus. Mauris vitae nisl nec metus placerat consectetuer. Donec ipsum. Proin imperdiet est. Phasellus dapibus semper urna. Pellentesque ornare, orci in consectetuer hendrerit, urna elit eleifend nunc, ut consectetuer nisl felis ac diam. Etiam non felis. Donec ut ante. In id eros. Suspendisse lacus turpis, cursus egestas at sem. Phasellus pellentesque. Mauris quam enim, molestie in, rhoncus ut, lobortis a, est.</p>';
 
     public function init() {
         $this->_helper->layout->setLayout('main');
-       
     }
     
     public function indexAction() {
-        
+        $this->view->assign(array('text' => $this->_testo)
+                );
     }
     
     
      public function viewstaticAction() {
-       // $page = $this->_getParam('staticPage');
+        $page = $this->_getParam('staticPage');
         $this->render($page);
     }
 }
