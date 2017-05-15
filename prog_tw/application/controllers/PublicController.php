05e443db7e96d7246a91edb0bf4c4f27c3068bdc
alexcam96 <s1071182@studenti.univpm.it>
2017-05-15 11:18:47 +0200

    aggiuntaCommentiCodice

--- prog_tw/application/controllers/PublicController.php
+++ prog_tw/application/controllers/PublicController.php
@@ -5,6 +5,7 @@ class PublicController extends Zend_Controller_Action {
     protected $_testo = '<p>Sed lacus. Donec lectus. Nullam pretium nibh ut turpis. Nam bibendum. In nulla tortor, elementum vel, tempor at, varius non, purus. Mauris vitae nisl nec metus placerat consectetuer. Donec ipsum. Proin imperdiet est. Phasellus dapibus semper urna. Pellentesque ornare, orci in consectetuer hendrerit, urna elit eleifend nunc, ut consectetuer nisl felis ac diam. Etiam non felis. Donec ut ante. In id eros. Suspendisse lacus turpis, cursus egestas at sem. Phasellus pellentesque. Mauris quam enim, molestie in, rhoncus ut, lobortis a, est. </p><p>Sed lacus. Donec lectus. Nullam pretium nibh ut turpis. Nam bibendum. In nulla tortor, elementum vel, tempor at, varius non, purus. Mauris vitae nisl nec metus placerat consectetuer. Donec ipsum. Proin imperdiet est. Phasellus dapibus semper urna. Pellentesque ornare, orci in consectetuer hendrerit, urna elit eleifend nunc, ut consectetuer nisl felis ac diam. Etiam non felis. Donec ut ante. In id eros. Suspendisse lacus turpis, cursus egestas at sem. Phasellus pellentesque. Mauris quam enim, molestie in, rhoncus ut, lobortis a, est.</p>';
 
     public function init() {
+        //imposto come layouy il file main.phtml
         $this->_helper->layout->setLayout('main');
     }
     
@@ -14,6 +15,11 @@ class PublicController extends Zend_Controller_Action {
     
     
      public function viewstaticAction() {
+         //mi permette di impostare la view da visualizare
+         //di default viene visualizzata la vista con il nome dell'action
+         //in questo caso il viewHelper render impone una vista diversa
+         //così posso parametrizzare la scelta della vista che sarà in questo caso
+         //who o where
         $page = $this->_getParam('staticPage');
         $this->render($page);
     }
