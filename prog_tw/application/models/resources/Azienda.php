90a123bedad864255ca81759fe7adfbd40cc2de2
martipio <s1073531@studenti.univpm.it>
2017-05-22 19:53:47 +0200

    Creazione file model per DB
    Praticamente ho preso i file dell'esempio del prof e ho cambiato i nomi
    adattandoli al nostro DB

---
+++ prog_tw/application/models/resources/Azienda.php
@@ -0,0 +1,14 @@
+<?php
+
+class Application_Resource_Azienda extends Zend_Db_Table_Abstract
+{
+    protected $_name    = 'azienda'; //**nome preciso della tabella
+    protected $_primary  = 'ID_Azienda';    //**nome preciso chiave primaria
+    protected $_rowClass = 'Application_Resource_Azienda_Item'; //**dove prende i record della tabella
+    
+	public function init()
+    {
+    }
+      
+}
+
---
+++ prog_tw/application/models/resources/Promozione/Item.php
@@ -0,0 +1,16 @@
+<?php
+
+class Application_Resource_Promozione_Item extends Zend_Db_Table_Row_Abstract
+{   
+	public function getPrice($withDiscount=false)
+    {
+    	$price = $this->price;
+    	if (true == $this->isDiscounted() && true == $withDiscount) {
+            $discount = $this->discountPerc;
+            $discounted = ($price*$discount)/100;
+            $price = round($price - $discounted, 2);
+        }
+        return $price;
+    }
+    
+}
