532fe33364c4663c746638b764fb79e3778ea363
alexcam96 <s1071182@studenti.univpm.it>
2017-06-12 18:27:46 +0200

    ok

--- prog_tw/application/views/helpers/Ottieni.php
+++ prog_tw/application/views/helpers/Ottieni.php
@@ -1,28 +1,19 @@
 <?php
-
 class Zend_View_Helper_Ottieni extends Zend_View_Helper_Abstract {
 
     public function Ottieni($idPromo) {
         $this->_authService = new Application_Service_Auth();
         $datiUtente = $this->_authService->getIdentity();
-        if (isset($datiUtente->role)) {
-            $u = $datiUtente->role;
-            
-            if($u == "user") {
-            $idUtente = $datiUtente->ID_Utente;
 
-            $urlaction = $this->view->url(array('controller' => 'user', 'action' => 'addcoupon', 'idPromo' => $idPromo, 'idUtente' => $idUtente));
+        if (isset($datiUtente->role)) {
+            $idUtente = $datiUtente->ID_Utente;
+            
+            $urlaction = $this->view->url(array('controller'=>'user', 'action'=>'addcoupon', 'idPromo'=>$idPromo, 'idUtente'=>$idUtente));
 
             $form = '<form action="' . $urlaction . '"><input type="submit" value="Ottieni" id="submit_buy"></form>';
             return $form;
         } else {
             return;
         }
-        }
-        else
-        {
-            return;
-        }
     }
-
 }
